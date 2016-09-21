f8637023b7b1aacaba80e31a6496f8160c97cca4
vinceajy93 <vinceajy93@hotmail.com>
2016-09-21 18:15:39 +0800

    deleted some stuffs added a standard prefab for canvas and camera

---
+++ FYPFramework/Assets/Turret Game/Scripts/Bullet_Movement.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet_Movement : MonoBehaviour {
+	public GameObject Bg;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//dt += Time.deltaTime;
+		//transform.Rotate (0, 0, -Time.deltaTime * 500);
+
+		//transform.localPosition += Vector3.up * Time.deltaTime * 10;
+		if (gameObject.tag == "Bullet_1")
+			transform.localPosition += Vector3.up * Time.deltaTime * 200;
+		if(gameObject.tag == "Bullet_2")
+			this.transform.localPosition += Vector3.down * Time.deltaTime * 70;
+
+		//check if bullet is out of gameplay screen
+		//Debug.Log(transform.position);
+		Camera cam = Camera.main;
+		float height = 2f * cam.orthographicSize;
+		float width = height * cam.aspect;
+
+		Vector2 sprite_size = Bg.GetComponent<SpriteRenderer> ().sprite.rect.size;
+		Vector2 local_sprite_size = sprite_size / Bg.GetComponent<SpriteRenderer> ().sprite.pixelsPerUnit;
+		Vector3 world_size = local_sprite_size;
+		world_size.x *= Bg.transform.lossyScale.x;
+		world_size.y *= Bg.transform.lossyScale.y;
+
+		if (transform.position.y > world_size.y && gameObject.tag == "Bullet_1") {
+			Destroy (gameObject);
+			int total = PlayerPrefs.GetInt ("NoBullet");
+			PlayerPrefs.SetInt ("NoBullet", total - 1);
+		}
+	}
+	void OnCollisionEnter2D( Collision2D other){
+		Debug.Log ("works");
+		if(other.gameObject.tag == "Bullet_1"){
+			Destroy (gameObject);
+			Destroy (other.gameObject);
+		}
+	}
+}
---
+++ FYPFramework/Assets/Turret Game/Scripts/CharacterControl.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+//[RequireComponent(typeof(CircleCollider2D))]
+
+public class CharacterControl : MonoBehaviour {
+	private Vector3 screenPoint;
+	private Vector3 offset;
+
+	private bool onPointerDownL = false;
+	private bool onPointerDownR = false;
+	private bool isInCanvas = false;
+
+	public Canvas canvas;
+	//Start is called at the start of the script
+	void Start(){
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.W)) {
+			Debug.Log ("transform pos" + this.transform.localPosition.x);
+			Debug.Log ("Screen width" + Screen.width);
+		}
+
+		Camera cam = Camera.main;
+		float height = 2f * cam.orthographicSize;
+		float width = height * cam.aspect;
+
+		// Get Background Size in worldspace
+		GameObject player_sprite = GameObject.Find("Player1");
+		Vector2 sprite_size = player_sprite.GetComponent<SpriteRenderer> ().sprite.rect.size;
+		Vector2 local_sprite_size = sprite_size / player_sprite.GetComponent<SpriteRenderer> ().sprite.pixelsPerUnit;
+		Vector3 player_world_size = local_sprite_size;
+		player_world_size.x *= player_sprite.transform.lossyScale.x;
+		player_world_size.y *= player_sprite.transform.lossyScale.y;
+
+		if(PlayerPrefs.HasKey("Control")){
+			// Move turret
+			if (PlayerPrefs.GetInt ("Control") == 0) {
+				GameObject player = GameObject.Find ("Player1");
+				if (onPointerDownL) {
+					if (player.transform.localPosition.x >= -width / 2 + player_world_size.x / 2) {
+						player.transform.localPosition += Vector3.left * Time.deltaTime * 5;
+					}
+				}
+
+				if (onPointerDownR) {
+					if (player.transform.localPosition.x <= width / 2 - player_world_size.x / 2) {
+						player.transform.localPosition += Vector3.right * Time.deltaTime * 5;
+					}
+				}
+			} else { // Move bullet
+				int total = PlayerPrefs.GetInt ("NoBullet");
+				int counter = 1;
+				counter = 1;
+				foreach (GameObject player_1 in GameObject.FindGameObjectsWithTag("Bullet_1")) {
+					if (total == counter) {
+						if (onPointerDownL) {
+							if (player_1.transform.position.x >= 10f) {
+								player_1.transform.position += Vector3.left * Time.deltaTime * 200;
+							}
+						}
+
+						if (onPointerDownR) {
+							if (player_1.transform.position.x <= width - 10f) {
+								player_1.transform.position += Vector3.right * Time.deltaTime * 200;
+							}
+						}
+						break;
+					} else
+						counter++;
+				}
+			}
+		}
+	}
+
+	/*public void OnMouseDown(){
+		screenPoint = Camera.main.WorldToScreenPoint (gameObject.transform.position);
+		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y));
+	}
+
+	public void OnMouseDrag(){
+		Vector3 curScreenPoint = new Vector3 (Input.mousePosition.x, Input.mousePosition.y);
+		//Vector3 curScreenPoint = new Vector3 (Input.mousePosition.x, -3.5f);
+		Vector3 curPosition = Camera.main.ScreenToWorldPoint (curScreenPoint) + offset;
+		this.transform.position = curPosition;
+	}*/
+
+	public void onButtonDownLeft(){
+		onPointerDownL = true;
+	}
+
+	public void onButtonDownRight(){
+		onPointerDownR = true;
+	}
+
+	public void onButtonUpLeft(){
+		onPointerDownL = false;
+	}
+
+	public void onButtonUpRight(){
+		onPointerDownR = false;
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		Debug.Log ("working");
+		if (other.gameObject.tag == "Canvas") {
+			isInCanvas = true;
+		} else {
+			isInCanvas = false;
+		}
+	}
+}
