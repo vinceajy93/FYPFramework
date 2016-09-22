72c581d6e583a5edbd3b1640091e2737310acd44
vinceajy93 <vinceajy93@hotmail.com>
2016-09-22 15:26:13 +0800

    changs log ------------------- -Added transtions -Added new scenes (main menu, splash, mode selection, loadout, logbook, options) -Added splash scene animation
    scripts
    -------------------
    -Added levelcontroller script

---
+++ FYPFramework/Assets/Turret Game/Scripts/LevelController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LevelController : MonoBehaviour {
+
+	public Text debugText;
+
+	// Use this for initialization
+	void Start () {
+		debugText.text = ("Stage: " + SceneManager.GetActiveScene().name);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	//load the main menu scene
+	public void loadMenu(){
+		SceneManager.LoadScene ("Main Menu");
+	}
+
+	//load the play scene
+	public void loadPlay(){
+		SceneManager.LoadScene ("Mode Selection");
+	}
+
+	//load the loadout scene
+	public void loadLoadout(){
+		SceneManager.LoadScene ("Loadout");
+	}
+
+	//load the logbook scene
+	public void loadLogbook(){
+		SceneManager.LoadScene ("Logbook");
+	}
+
+	//load the tutorial selection scene
+	public void loadTutorialSelect(){
+		SceneManager.LoadScene ("Tutorial Selection");
+	}
+
+	//load the options scene
+	public void loadOptions(){
+		SceneManager.LoadScene ("Options");
+	}
+}
---
+++ FYPFramework/Assets/Turret Game/Scripts/Splash/SplashLoader.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SplashLoader : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		//PlayerPrefs.SetFloat ("curVol", 1.0f);
+		StartCoroutine ("Countdown");
+	}
+	
+	private IEnumerator Countdown(){
+		yield return new WaitForSeconds (6);
+		//Application.LoadLevel ("main menu");
+		SceneManager.LoadScene("Main Menu");
+	}
+}
