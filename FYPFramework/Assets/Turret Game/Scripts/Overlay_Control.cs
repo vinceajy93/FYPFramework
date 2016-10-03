59184796e75359f173b2b993e58838e00872c7c8
vinceajy93 <vinceajy93@hotmail.com>
2016-10-03 17:29:54 +0800

    Added/Edited the overlay control to support single player

--- FYPFramework/Assets/Turret Game/Scripts/Overlay_Control.cs
+++ FYPFramework/Assets/Turret Game/Scripts/Overlay_Control.cs
@@ -8,7 +8,7 @@ public class Overlay_Control : MonoBehaviour
 	private GameObject panel;
 	private Text p1Timer;
 	private Text p2Timer;
-	float countdownTimer = 3.5f;
+	float countdownTimer = 0.5f; //change this time for debugging
 	public bool PanelisActive = true;
 	private bool P1Touched, P2Touched = false;
 
@@ -20,17 +20,19 @@ public class Overlay_Control : MonoBehaviour
 		//single player
 		if (gameObject.GetComponent<Mode_Control> ().game_mode_Single) {
 			p1Timer = GameObject.Find ("P1 Timer").GetComponent<Text> ();
-
+			p1Timer.text = ("Player 1 place finger here!");
 		} else {
 			//multiplayer
 			p1Timer = GameObject.Find ("P1 Timer").GetComponent<Text> ();
 			p2Timer = GameObject.Find ("P2 Timer").GetComponent<Text> ();
 
+			p1Timer.text = ("Player 1 place finger here!");
+			p2Timer.text = ("Player 2 place finger here!");
 		}
 
 
-		p1Timer.text = ("Player 1 place finger here!");
-		p2Timer.text = ("Player 2 place finger here!");
+
+
 	}
 	
 	// Update is called once per frame
@@ -39,17 +41,31 @@ public class Overlay_Control : MonoBehaviour
 		//start countdown timer only when finger is on screen
 		CheckPress ();
 
-		if (P1Touched == true && P2Touched == true) {
+		//single player
+		if (gameObject.GetComponent<Mode_Control> ().game_mode_Single) {
+			if (P1Touched == true) {
 			
-			//countdown timer
-			countdownTimer -= Time.deltaTime;
-			//Set Panel to false when timer hits 0
-			if (countdownTimer <= 0)
-				PanelisActive = false;
+				//countdown timer
+				countdownTimer -= Time.deltaTime;
+				//Set Panel to false when timer hits 0
+				if (countdownTimer <= 0)
+					PanelisActive = false;
 
-			Panel ();
+				Panel ();
+			}
+			//multiplayer (2P)
+		} else {
+			if (P1Touched == true && P2Touched == true) {
+				
+				//countdown timer
+				countdownTimer -= Time.deltaTime;
+				//Set Panel to false when timer hits 0
+				if (countdownTimer <= 0)
+					PanelisActive = false;
+
+				Panel ();
+			}
 		}
-			
 	}
 
 	void Panel ()
@@ -57,8 +73,8 @@ public class Overlay_Control : MonoBehaviour
 		if (PanelisActive == true) {
 			//single player
 			if (gameObject.GetComponent<Mode_Control> ().game_mode_Single) {
-				p1Timer.text = countdownTimer.ToString ("f0");
 				p1Timer.GetComponent<Text> ().fontSize = 120;
+				p1Timer.text = countdownTimer.ToString ("f0");
 
 			}
 
@@ -79,42 +95,59 @@ public class Overlay_Control : MonoBehaviour
 
 	void CheckPress ()
 	{
-		if (gameObject.GetComponent<Mode_Control> ().game_mode_Single) {
-			//single player
-		} else {
-			//multiplayer
 
+		int nbTouches = Input.touchCount;
 
-			int nbTouches = Input.touchCount;
+		if (nbTouches > 0) {
+			for (int i = 0; i < nbTouches; i++) {
+				Touch touch = Input.GetTouch (i);
 
-			if (nbTouches > 0) {
-				for (int i = 0; i < nbTouches; i++) {
-					Touch touch = Input.GetTouch (i);
+				TouchPhase phase = touch.phase;
 
-					TouchPhase phase = touch.phase;
+				switch (phase) {
+				case TouchPhase.Began:
+					Vector2 touchPosition = Camera.main.ScreenToWorldPoint (touch.position);
 
-					switch (phase) {
-					case TouchPhase.Began:
-						Vector2 touchPosition = Camera.main.ScreenToWorldPoint (touch.position);
+						//single player
+					if (gameObject.GetComponent<Mode_Control> ().game_mode_Single) {
+						
+						//player 1 touched
+						if (touchPosition.y < 0) {
+							P1Touched = true;
+							p1Timer.text = ("waiting for player 2!");
+						}
+					}
+						//multiplayer
+						else {
 
 						//player 1 touched
 						if (touchPosition.y < 0) {
 							P1Touched = true;
 							p1Timer.text = ("waiting for player 2!");
 						}
-							
-
+								
 						//player 2 touched
 						if (touchPosition.y > 0) {
 							P2Touched = true;
 							p2Timer.text = ("waiting for player 1!");
 						}
-													
-				
-						break;
-					
-					case TouchPhase.Ended: // Release touch from screen
+					}
 
+					break;
+					
+				case TouchPhase.Ended: // Release touch from screen
+
+						//single player
+					if (gameObject.GetComponent<Mode_Control> ().game_mode_Single) {
+						p1Timer.text = ("Place finger here to start!");
+
+						if (P1Touched) {
+						} else { //not touching 
+							P1Touched = false;
+						}
+					}
+						//multiplayer
+						else {
 						p1Timer.text = ("Player 1 place finger here!");
 						p2Timer.text = ("Player 2 place finger here!");
 
@@ -123,12 +156,11 @@ public class Overlay_Control : MonoBehaviour
 							P1Touched = false;
 							P2Touched = false;
 						}
-
-						break;
 					}
+
+					break;
 				}
 			}
 		}
 	}
 }
-
